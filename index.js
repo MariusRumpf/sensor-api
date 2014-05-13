cabb6658f91e69867d6ddde6a7e8b7435aa98c09
MariusRumpf <marius@mariusrumpf.de>
2014-05-13 22:55:29 +0200

    DHT 22 development beginning

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ require('log-timestamp');
 /**
  * Your sensor config goes here
  */
-var TMP35 = require('./lib/controller/tmp35.js');
+var TMP35 = require('./lib/controller/tmp35.js'),
+    // DHT22 = require('./lib/controller/dht22.js');
 
 var tempSensor = new TMP35({
   'sensorName': 'temperatur-wohnzimmer',
@@ -26,10 +27,24 @@ var tempSensor = new TMP35({
   'database' : true
 });
 
-
 setInterval(function() {
   tempSensor.read(function(err, value) {
     if(err) throw err;
     console.log('Temperatur', value + '°C');
   });
 }, 60000);
+
+// var humidSensor = new DHT22({
+//   'sensorName': 'dht22-wohnzimmer',
+// });
+//
+// setInterval(function() {
+//     humidSensor.read(function(err, value) {
+//       if(err) {
+//         console.error(err);
+//       } else {
+//         console.log(value.temp + '°C');
+//         console.log(value.hum + '%');
+//       }
+//     });
+// }, 7000);
