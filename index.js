58b32bfad34d2e6d10044ddb9414b43ea5e4856c
MariusRumpf <marius@mariusrumpf.de>
2014-06-11 23:35:47 +0200

    Add Photodiode support, better type ensurance

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ require('log-timestamp');
  * Your sensor config goes here
  */
 var TMP35 = require('./lib/controller/tmp35.js'),
-    DHT22 = require('./lib/controller/dht22.js');
+    DHT22 = require('./lib/controller/dht22.js'),
+    Photodiode = require('./lib/controller/photodiode.js');
 
 var tempSensor = new TMP35({
   'sensorName': 'tmp35-wohnzimmer',
@@ -26,6 +27,19 @@ var tempSensor = new TMP35({
   'rVoltage': 3.27,
   'database' : true
 });
+
+var humidSensor = new DHT22({
+  'sensorName': 'dht22-wohnzimmer',
+  'pin': 4,
+  'database': true
+});
+
+var lightSensor = new Photodiode({
+  'sensorName': 'photodiode1',
+  'pin': 1,
+  'database': true
+});
+
 
 setInterval(function() {
   tempSensor.read(function(err) {
@@ -35,16 +49,18 @@ setInterval(function() {
   });
 }, 2000);
 
-var humidSensor = new DHT22({
-  'sensorName': 'dht22-wohnzimmer',
-  'pin': 4,
-  'database': true
-});
-
 setInterval(function() {
     humidSensor.read(function(err) {
       if(err) {
         console.error(err);
       }
     });
-}, 60000);
+}, 2000);
+
+setInterval(function() {
+    lightSensor.read(function(err) {
+      if(err) {
+        console.error(err);
+      }
+    });
+}, 2000);
